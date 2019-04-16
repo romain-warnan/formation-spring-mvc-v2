02621c10aec18c51b28adf598df644fef47df81a
Romain Warnan <romain.warnan@insee.fr>
2019-04-16 08:46:20 +0200

    Déplacement des tests unitaires

--- src/test/java/fr/insee/springmvc/controller/AccueilControllerTest.java
+++ src/test/java/fr/insee/springmvc/controller/AccueilControllerTest.java
@@ -1,4 +1,4 @@
-package fr.insee.springmvc;
+package fr.insee.springmvc.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
